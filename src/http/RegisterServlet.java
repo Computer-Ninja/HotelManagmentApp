a5ae6cd3d05eb4a83cd9fdf2aab4918d5fad3028
Sithira Munasinghe <sithiraac@gmail.com>
2019-10-06 16:15:18 +0530

    Fixes

--- src/http/RegisterServlet.java
+++ src/http/RegisterServlet.java
@@ -1,11 +1,16 @@
 package http;
 
+import config.ConnectionManager;
+import http.utils.AuthUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
@@ -19,19 +24,45 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
 
-
-        String frname = req.getParameter("fname");
-        String lname = req.getParameter("lname");
+        String frname = req.getParameter("fName");
+        String lname = req.getParameter("lName");
         String address = req.getParameter("address");
         String phone = req.getParameter("phone");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         String repassword = req.getParameter("repassword");
 
-//        String sql=  " INSERT INTO `users`(`email`, `first_name`, `last_name`, `address`, `phone`, `password`) VALUES ()";
+        if (!password.equals(repassword)) {
+            System.out.println("Failed. Passwords does not match....");
+            return;
+        }
 
+        String sql = "INSERT INTO `users`(`email`, `first_name`, `last_name`, `address`, `phone`, `password`) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement statement = ConnectionManager.getInstance().getConnection().prepareStatement(sql);
+
+            statement.setString(1, email);
+            statement.setString(2, frname);
+            statement.setString(3, lname);
+            statement.setString(4, address);
+            statement.setString(5, phone);
+            statement.setString(6, password);
+
+            int status = statement.executeUpdate();
+
+            if (status == 1) {
+                System.out.println("Inserted...");
+
+                AuthUtils.doLogin(req, resp, email);
+
+                return;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/http/utils/AuthUtils.java
+++ src/http/utils/AuthUtils.java
@@ -2,6 +2,8 @@ package http.utils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class AuthUtils {
 
@@ -30,4 +32,19 @@ public class AuthUtils {
         return userId;
     }
 
+    public static HttpServletResponse doLogin(HttpServletRequest request, HttpServletResponse response, String userId) {
+        response.addCookie(new Cookie("AUTH", userId));
+
+        AuthUtils.userId = userId;
+
+        try {
+            response.sendRedirect(request.getContextPath() + "/dashboard");
+
+            return response;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
 }
